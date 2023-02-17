c78cbe97718a98312a01b5ee8727fe22c03e025a
slice_of_code <slice_of_code@gmail.com>
2023-02-17 19:23:18 +0400

    without layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Country} from "./Country";
+
+export type BanknotsType = 'Dollars' | 'RUBLS' | "All"
+
+export type MoneyType = {
+    banknotes: BanknotsType
+    value: number
+    number: string
+}
+
+let defaultMoney: MoneyType[] = [  // 1. типизируем
+    {banknotes: 'Dollars', value: 100, number: ' a1234567890'},
+    {banknotes: 'Dollars', value: 50, number: ' z1234567890'},
+    {banknotes: 'RUBLS', value: 100, number: ' w1234567890'},
+    {banknotes: 'Dollars', value: 100, number: ' e1234567890'},
+    {banknotes: 'Dollars', value: 50, number: ' c1234567890'},
+    {banknotes: 'RUBLS', value: 100, number: ' r1234567890'},
+    {banknotes: 'Dollars', value: 50, number: ' x1234567890'},
+    {banknotes: 'RUBLS', value: 50, number: ' v1234567890'},
+]
+
+
+export const moneyFilter = (money: MoneyType[], filterValue: BanknotsType): MoneyType[] => {
+    if (filterValue === "All") return money
+    return money.filter((el) => el.banknotes === filterValue)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [money, setMoney] = useState<MoneyType[]>(defaultMoney) // 1. Типизируем стейт и указываем
+    // оригинальный массив в качестве начального значения
+    const [filterValue, setFilterValue] = useState<BanknotsType>('All') // 2. Типизируем
+    // стейт и указываем начальное значение
+    const filteredMoney = moneyFilter(money, filterValue) // 3. Присваиваем переменной отфильтрованный
+    // массив
+
+    return (
+        <div className="App">
+            <Country
+                data={filteredMoney}
+                setFilterValue={setFilterValue} // 4. Передаём стейт в компоненту
+
+            />
+        </div>
+    );
 }
 
 export default App;
